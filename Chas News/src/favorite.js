e5cdf8fae66cc331bf4a65ec31b899a5a48ef78c
Lilikh <lilikheirandish051@gmail.com>
2024-02-12 21:26:31 +0100

    add line on accoun in log in

--- Chas News/src/favorite.js
+++ Chas News/src/favorite.js
@@ -119,6 +119,7 @@ export const renderFavorites = () => {
             <div id="titleList" style="display: flex; flex-direction: column; background-color: #fff; border: 1px solid #ccc; position: absolute; z-index: 1;">
               <p style="margin:1rem;font-style: bold; font-weight: 600; cursor: pointer;"> <i class="fa-regular fa-star"style="padding-right:2rem; " ></i>Sparade artiklar</p> 
               <p style="margin: 1rem;font-style: bold; font-weight: 600; cursor: pointer;"><i class="fa-regular fa-clock" style="padding-right:2rem;"></i>Läs historik</p>
+              <hr>
               <p style="margin:1rem;font-style: bold;font-weight: 600; cursor: pointer;"> <i class="fa-regular fa-bell" style="padding-right:2rem;"></i>Push noticer</p>
               <button id="logoutBtn" style="margin-bottom:0.8rem; margin-right:2rem; background-color:#DE667B; color: #fff; border: none; width:150px;padding-left:6px; border-radius: 5px;"> <i class="fa-solid fa-arrow-right-from-bracket" style="padding-right:2rem;"></i>Logout</button>
               
